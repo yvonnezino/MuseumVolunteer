4ac262ab1d0b683bbe0a93d9e3639f2ea7560fdc
Yvonne Zino <y_zino@coloradocollege.edu>
2020-05-04 13:39:30 -0700

    connection microtest

---
+++ MiniTests.java
@@ -0,0 +1,32 @@
+package MuseumVolunteer;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MiniTests {
+    Connection connection;
+
+    @BeforeEach
+    public void before() {
+        connection = Graph.connect();
+    }
+
+    @AfterEach
+    public void after() {
+        Graph.connect();
+    }
+    @Test
+    public void closeStatementShouldCloseStatement() throws SQLException {
+        Statement statement = connection.createStatement();
+        Graph.closeStatement(statement);
+        assertTrue(statement.isClosed());
+    }
+
+}
